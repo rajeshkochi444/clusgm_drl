e7ae5c9458de9428b2b5ab5c64320f1a51b32a68
Rajesh <rajesh444@gmail.com>
2024-05-16 22:13:16 -0400

    modified slurm script

--- zgym_single.sh
+++ zgym_single.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -l
+#SBATCH --partition=long
 #SBATCH --job-name=drlclus
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --get-user-env
 #SBATCH --export=NONE
-#SBATCH --time=240:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu 4G
 
+set -e  # exit on error.
+echo "Date:     $(date)"
+echo "Hostname: $(hostname)"
 
-module purge
-module load bluebear
+module --quiet purge
 
 cd "$PBS_O_WORKDIR"
 export OMP_NUM_THREADS=1
@@ -23,13 +26,13 @@ echo "working directory = "$SLURM_SUBMIT_DIR
 
 
 #source /rds/projects/2018/johnston-copper-clusters-rr/Rajesh-2/anaconda3/etc/profile.d/conda.sh
-source /rds/homes/r/rajur/mambaforge-pypy3/etc/profile.d/conda.sh
-conda activate clusgym_dscribe
+source /network/scratch/r/rajesh.raju/miniforge3/etc/profile.d/conda.sh
+conda activate clusgym_small
 
 
 echo "Starting at "`date`
 #mpirun  gym_trpo_parallel_training_ver50_expt1.py
-python  gym_trpo_single_ver101_new-2.py
+python  gym_trpo_single.py
 echo "Ending at "`date`
 
 
